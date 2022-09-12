ec0d5cff1c88e0a51914049ac0ee4d1a2c95fac9
Selina252000 <selina.darnal@netsmartz.net>
2022-09-12 12:01:50 +0530

    adding the files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import {Form,Button,Checkbox,Input, Row,Col} from "antd";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+    <Row>
+      <Col span={10} offset = {7} > 
+
+      <Form 
+      autoComplete="off"
+      labelCol={{span:10}} 
+      wrapperCol={{span:14}}
+      >
+
+      <Form.Item 
+      name ="firstName" 
+      label ="firstName"
+      rules = {[
+        {
+          required:true,
+          message:"Enter your first  name",
+        },
+        {whitespace:true},
+        {min:3}
+
+      ]}
+       hasFeedback
+       >
+        <Input placeholder="Type your first name"/>
+      </Form.Item>
+
+      <Form.Item 
+      name ="lastName" 
+      label ="lastName" 
+      rules = {[
+        {
+          required:true,
+          message:"Enter your last  name",
+        },
+        {whitespace:true},
+
+      ]}
+      hasFeedback
+      >
+        <Input placeholder="Type your last name"/>
+      </Form.Item>
+
+      <Form.Item
+       name ="email" 
+       label ="email"
+       rules = {[
+        {
+          required:true,
+          message:"Enter your email",
+        },
+        {type:"email",message:"enter valid email"},
+      ]}
+        hasFeedback
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Input placeholder="Type your email"/>
+      </Form.Item>
+
+      <Form.Item 
+      name ="password" 
+      label ="password" 
+      rules = {[
+        {
+          required:true,
+        },
+        {min:6},
+        // {
+        //   validator:(_,value)=>
+        //   value && value.includes("A")
+        //   ? Promise.resolve()
+        //   :Promise.reject("password does not match the critria")
+        // }
+
+      ]}
+      hasFeedback
+      >
+        <Input.Password placeholder="Type your password"/>
+      </Form.Item>
+
+      <Form.Item 
+      name ="confirmpassword"
+       label ="confirmpassword" 
+       dependencies={['password']}
+       rules = {[
+        {
+          required:true,
+        },
+        ({getFieldValue})=>({
+          validator(_,value){
+           if(!value|| getFieldValue('password')=== value){
+            return Promise.resolve()
+           } 
+           return Promise.reject("Password did not match")
+          }
+
+        })
+      ]}
+       hasFeedback>
+        <Input.Password placeholder="confirm your password"/>
+      </Form.Item>
+
+      <Form.Item name ="agreement" wrapperCol={{span:24}}  hasFeedback>
+        {" "}
+        <Checkbox > Agree to  our <a href ='a'>Terms and condition</a>
+        </Checkbox>
+      </Form.Item>
+
+      <Form.Item  wrapperCol={{span:24}} >
+        <Button block type ="primary" htmlType='submit'>
+          Register
+        </Button>
+      </Form.Item>
+
+      </Form>
+      </Col>
+    </Row>
+    </>
   );
 }
 
